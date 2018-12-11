9e411241542f538bb671454ce2ea0930ad07945a
Raghu Betina <raghu@firstdraft.com>
2018-12-11 03:57:23 +0000

    Generate matches resource with draft:resource

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,63 @@
+class MatchesController < ApplicationController
+  def index
+    @matches = Match.all
+
+    render("match_templates/index.html.erb")
+  end
+
+  def show
+    @match = Match.find(params.fetch("id_to_display"))
+
+    render("match_templates/show.html.erb")
+  end
+
+  def new_form
+    @match = Match.new
+
+    render("match_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @match = Match.new
+
+    @match.user_id = params.fetch("user_id")
+    @match.other_id = params.fetch("other_id")
+
+    if @match.valid?
+      @match.save
+
+      redirect_back(:fallback_location => "/matches", :notice => "Match created successfully.")
+    else
+      render("match_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @match = Match.find(params.fetch("prefill_with_id"))
+
+    render("match_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @match = Match.find(params.fetch("id_to_modify"))
+
+    @match.user_id = params.fetch("user_id")
+    @match.other_id = params.fetch("other_id")
+
+    if @match.valid?
+      @match.save
+
+      redirect_to("/matches/#{@match.id}", :notice => "Match updated successfully.")
+    else
+      render("match_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @match = Match.find(params.fetch("id_to_remove"))
+
+    @match.destroy
+
+    redirect_to("/matches", :notice => "Match deleted successfully.")
+  end
+end
