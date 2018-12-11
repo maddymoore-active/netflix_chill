524fdbafdde6d2be48c868d85e7b1889e540f634
Raghu Betina <raghu@firstdraft.com>
2018-12-11 03:57:44 +0000

    Whitelist columns in Show for ActiveAdmin

--- app/admin/shows.rb
+++ app/admin/shows.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Show do
+
+ permit_params :name, :title, :image
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
